594ca1f78d8c3cfeeb75a599795a7e12a0a05a98
zubayerhimel <zhimel007@gmail.com>
2023-10-02 15:32:06 +0600

    style: cleared login page design

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -39,17 +39,6 @@ export default function Login() {
               <p className='text-sm text-muted-foreground'>Enter your email below to create your account</p>
             </div>
             <UserAuthForm />
-            <p className='px-8 text-sm text-center text-muted-foreground'>
-              By clicking continue, you agree to our{' '}
-              <Link href='/terms' className='underline underline-offset-4 hover:text-primary'>
-                Terms of Service
-              </Link>{' '}
-              and{' '}
-              <Link href='/privacy' className='underline underline-offset-4 hover:text-primary'>
-                Privacy Policy
-              </Link>
-              .
-            </p>
           </div>
         </div>
       </div>
--- components/UserAuthForm.tsx
+++ components/UserAuthForm.tsx
@@ -32,20 +32,18 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
             </Label>
             <Input id='email' placeholder='name@example.com' type='email' autoCapitalize='none' autoComplete='email' autoCorrect='off' disabled={isLoading} />
           </div>
+          <div className='grid gap-1'>
+            <Label className='sr-only' htmlFor='password'>
+              Password
+            </Label>
+            <Input id='password' placeholder='Password' type='password' autoCapitalize='none' autoComplete='email' autoCorrect='off' disabled={isLoading} />
+          </div>
           <Button disabled={isLoading}>
             {isLoading && <UpdateIcon className='w-4 h-4 mr-2 animate-spin' />}
-            Sign In with Email
+            Sign In
           </Button>
         </div>
       </form>
-      <div className='relative'>
-        <div className='absolute inset-0 flex items-center'>
-          <span className='w-full border-t' />
-        </div>
-        <div className='relative flex justify-center text-xs uppercase'>
-          <span className='px-2 bg-background text-muted-foreground'>Or continue with</span>
-        </div>
-      </div>
     </div>
   );
 }
